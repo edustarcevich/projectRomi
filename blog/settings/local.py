090ba1c53c583e20220195cd0071aae848755bc5
edustarcevich <88065414+edustarcevich@users.noreply.github.com>
2022-08-25 12:19:55 -0300

    Update local.py

--- blog/settings/local.py
+++ blog/settings/local.py
@@ -31,7 +31,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER="cam.com.ch@outlook.com"
-EMAIL_HOST_PASSWORD = "27abj8Mh1"
+EMAIL_HOST_USER="un_email_de@outlook.com"
+EMAIL_HOST_PASSWORD = "password"
 
 
